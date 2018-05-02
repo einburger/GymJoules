03492d85286961f5a545ec3e1d0043b078a8c5e6
Chris Carter <ctobiasc@gmail.com>
2018-05-01 21:15:40 -0500

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
-import HomeScreen from './src/Home'
+import HomeScreen from './src/Home';
 import ExerciseSelectionScreen from './src/ExerciseSelection';
 import RecordExerciseDataScreen from './src/RecordExerciseData';
 
