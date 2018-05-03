b3905bba1e877158c707b9b56d0cc545a561957b
einburger <ctobiasc@gmail.com>
2018-05-03 00:39:15 -0500

    moved the .js files related to screens into a screen folder

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
-import HomeScreen from './src/Home'
-import ExerciseSelectionScreen from './src/ExerciseSelection';
-import RecordExerciseDataScreen from './src/RecordExerciseData';
+import HomeScreen from './src/screens/Home'
+import ExerciseSelectionScreen from './src/screens/ExerciseSelection';
+import RecordExerciseDataScreen from './src/screens/RecordExerciseData';
 
 
 const RootStack = StackNavigator(
