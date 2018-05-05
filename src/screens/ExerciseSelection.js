88601e11510e33d4bb22a913d05ab709f1e7297d
einburger <ctobiasc@gmail.com>
2018-05-05 17:49:30 -0500

    added new exercises

--- src/screens/ExerciseSelection.js
+++ src/screens/ExerciseSelection.js
@@ -15,19 +15,28 @@ export default class ExerciseSelectionScreen extends Component {
     onPress = () => {
         this.props.navigation.navigate('Record')
     }
+    onSearch = () => {
+    }
 
     render() {
         return (
             <View style={styles.container}>
+                <TextInput 
+                    style={styles.searchBar}
+                    placeholder='Search For Exercise'
+                    onChangeText={this.onSearch}
+                />
                 <FlatList
                     data={[
                         { path: require('../assets/arnold_curl.gif'), name: 'Arnold Curl', key: '0' },
                         { path: require('../assets/dumbell_military_press.gif'), name: 'Military Press', key: '1' },
+                        { path: require('../assets/skullcrushers.gif'), name: 'Skull Crushers', key: '2' },
+                        { path: require('../assets/bench_press.gif'), name: 'Bench Press', key: '3' },
                     ]}
                     renderItem={({ item }) =>
-                        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-evenly' }}>
+                        <View style={styles.buttonContainer}>
                             <TouchableOpacity onPress={this.onPress}>
-                                <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly' }}>
+                                <View style={styles.gif}>
                                     <Image source={item.path} />
                                     <Text fontSize='30'> {item.name} </Text>
                                 </View>
@@ -42,18 +51,29 @@ export default class ExerciseSelectionScreen extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        flexDirection: 'column',
-        backgroundColor: '#fff',
-        alignItems: 'center',
         justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#FFFFFF',
     },
     button: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
         backgroundColor: '#859a9b',
-        borderRadius: 20,
-        padding: 10,
-        shadowColor: '#303838',
     },
+    gif: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+    },
+    searchBar: {
+        textAlign: 'center',
+        height: 40,
+        borderColor: '#74999e',
+        backgroundColor: '#FFFFFF',
+    }
 });
 
 // skip this line if using Create React Native App
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-import { AppRegistry, Button, View, Text } from 'react-native';
+import { AppRegistry, Button, View, Text, StyleSheet } from 'react-native';
 
 export default class HomeScreen extends Component {
+  onPress = () => {
+    this.props.navigation.navigate('Exercises')
+  }
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button title='Add exercise to calculation' onPress={() => this.props.navigation.navigate('Exercises')} />
+      <View style={styles.mainView}>
+        <Button 
+          title='Add exercise to calculation' 
+          flex='1'
+          flexDirection='row'
+          color= '#74999e'
+          onPress={this.onPress} 
+        />
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#c1c4bc'
+  },
+  button: {
+    flex: 1,
+    flexDirection: 'row',
+    color: '#74999e'
+  }
+});
 // skip this line if using Create React Native App
 AppRegistry.registerComponent('JoulesGym', () => HomeScreen);
