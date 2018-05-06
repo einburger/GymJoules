bb5b2cbc453d2aee936e83161c66e56ebd370dc1
pussy_magnet <eyercoolguy@gmail.com>
2018-05-06 17:38:19 -0400

    llama

--- src/screens/RecordExerciseData.js
+++ src/screens/RecordExerciseData.js
@@ -3,13 +3,13 @@ import { Alert, AppRegistry, Button, View, Text } from 'react-native';
 
 export default class RecordExerciseDataScreen extends Component {
   
-  onPress = () => {
-    Alert.alert('Added to exercise to calculation')
-  }
+    addToCalc = () => {
+      Alert.alert(title="fucking llamas")
+    }
   render() {
     return (
       <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly' }}>
-        <Button title='Add to calculation' onPress={this.onPress} />
+        <Button title='Add to calculation' onPress={addToCalc} />
         <Button title='Select another exercise' onPress={() => this.props.navigation.navigate('Exercises')} />
         <Button title='See results' onPress={() => this.props.navigation.navigate('Home')} />
       </View>
