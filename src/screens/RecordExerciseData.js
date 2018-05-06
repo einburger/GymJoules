b64684079ac7719c9be4110289e75933b316c190
pussy_magnet <eyercoolguy@gmail.com>
2018-05-06 17:41:56 -0400

    llamas

--- src/screens/RecordExerciseData.js
+++ src/screens/RecordExerciseData.js
@@ -9,7 +9,7 @@ export default class RecordExerciseDataScreen extends Component {
   render() {
     return (
       <View style={{ flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly' }}>
-        <Button title='Add to calculation' onPress={addToCalc} />
+        <Button title='Add to calculation' onPress={this.addToCalc} />
         <Button title='Select another exercise' onPress={() => this.props.navigation.navigate('Exercises')} />
         <Button title='See results' onPress={() => this.props.navigation.navigate('Home')} />
       </View>
