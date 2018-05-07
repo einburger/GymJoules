ac4bf63bf3715e27e008011963a8cdab65c16c46
pussy_magnet <eyercoolguy@gmail.com>
2018-05-06 20:03:47 -0400

    '

---
+++ src/screens/Checkout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {
+    Alert,
+    AppRegistry,
+    StyleSheet,
+    TouchableOpacity,
+    Image,
+    View,
+    Text,
+    TextInput,
+    FlatList
+} from 'react-native';
+
