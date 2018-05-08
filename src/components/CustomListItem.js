56617bee4e9890e701c83235b09e5cf5074d0c81
einburger <ctobiasc@gmail.com>
2018-05-07 22:40:59 -0500

    moved switch up top

--- src/components/CustomListItem.js
+++ src/components/CustomListItem.js
@@ -8,21 +8,22 @@ const customlistitem = (props) => {
     return (
         <View style={props.exercise_button_wrapper}>
             <TouchableOpacity onPress={props.onPress}>
-                <View style={{backgroundColor: '#f2f2f2', 
+                <View style={{backgroundColor: 'white', 
                               borderColor: 'white', 
                               borderWidth: 5,
                               borderTopStartRadius: 5, 
-                              borderTopEndRadius: 5}} />
+                              borderTopEndRadius: 5}} >
+                    <Switch style={props.switch_style} onValueChange={() => valueChange()}/>
+                </View>
                 <View style={props.gif_style}>
                     <Image resizeMode='stretch' source={props.img_path}/>
                 </View>
             </TouchableOpacity>
-            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <View style={{flexDirection: 'row', justifyContent: 'center'}}>
                 <Text style={props.font_style}> 
                     {props.item_name}
                 </Text>
                 <View/>
-                <Switch style={props.switch_style} onValueChange={() => valueChange()}/>
             </View>
         </View>
     )
